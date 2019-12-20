25882379baa33a8e59d8d5589476b01d730533b5
Hugo Drivière <hugo.driviere@gmail.com>
2019-12-20 16:31:37 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "test-canal"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-stream" % "2.6.1",
+  "com.lightbend.akka" %% "akka-stream-alpakka-amqp" % "1.1.2",
+  "com.lightbend.akka" %% "akka-stream-alpakka-csv" % "1.1.2"
+)
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % "test"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-stream-testkit" % "2.6.1" % "test"
---
+++ src/test/scala/TestProducerApp.scala
@@ -0,0 +1,21 @@
+import ProducerApp._
+import akka.NotUsed
+import akka.stream.scaladsl.{Flow, Keep, Sink}
+import akka.util.ByteString
+import org.scalatest.FunSuite
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
+
+class TestProducerApp extends FunSuite {
+  test("Is the flow working as expected") {
+    val sink: Sink[ByteString, Future[String]] = Flow[ByteString].map(_.utf8String).toMat(Sink.fold("")(_ + " " + _))(Keep.right)
+
+    val future = comedyTitlesFlow("src/test/resources/title.basics.test.tsv").runWith(sink)
+
+    val result = Await.result(future, 3 seconds)
+    assert(result == " " + "Salome Mad" + " " + "Jarní sen starého mládence")
+  }
+
+}
